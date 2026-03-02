1bb10ec52723f6cc4953e237c10eb7ad61b6f816
GGgrey <740775822@qq.com>
2026-03-02 21:39:24 +0800

    script update

--- recipe/intuitor/run_intuitor_qwen2.5_7b.sh
+++ recipe/intuitor/run_intuitor_qwen2.5_7b.sh
@@ -1,13 +1,13 @@
 set -x
 
-export RAY_TMPDIR="/data/sunqiao/projects/verl_exp/"
+export RAY_TMPDIR="/workspace/verl_exp/"
 export RAY_memory_monitor_refresh_ms=0
 export WANDB_API_KEY=37f371d2968f35d69749ee52089583eb8e1f0cab
-export WANDB_DIR="/data/sunqiao/projects/verl_exp/"
+export WANDB_DIR="/workspace/verl_exp/"
 export WANDB_MODE=online
 export ACCELERATE_LOG_LEVEL=info
 export HYDRA_FULL_ERROR=1
-export CUDA_VISIBLE_DEVICES="0,1"
+export CUDA_VISIBLE_DEVICES="2,3,4,5"
 
 project_name='Intuitor'
 exp_name='Intuitor-Qwen2.5-7B'
@@ -33,8 +33,8 @@ RUNTIME_ENV=${RUNTIME_ENV:-"${WORKING_DIR}/verl/trainer/runtime_env.yaml"}
 NNODES=${NNODES:-1}
 
 # Paths
-RAY_DATA_HOME=${RAY_DATA_HOME:-"/data/sunqiao/projects/verl_exp"}
-MODEL_PATH=${MODEL_PATH:-"/data/sunqiao/projects/models/Qwen/Qwen2.5-7B"}
+RAY_DATA_HOME=${RAY_DATA_HOME:-"/workspace/verl_exp"}
+MODEL_PATH=${MODEL_PATH:-"/models/Qwen/Qwen2.5-7B"}
 CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/ckpts/${project_name}/${exp_name}"}
 TRAIN_FILE=${TRAIN_FILE:-"${RAY_DATA_HOME}/data/math/train.parquet"}
 TEST_FILE=${TEST_FILE:-"${RAY_DATA_HOME}/data/math/test.parquet"}
@@ -70,18 +70,18 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
-    actor_rollout_ref.actor.fsdp_config.param_offload=${offload} \
-    actor_rollout_ref.actor.fsdp_config.optimizer_offload=${offload} \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.25 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.20 \
     actor_rollout_ref.rollout.n=${n_resp_per_prompt} \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
-    actor_rollout_ref.ref.fsdp_config.param_offload=${offload} \
+    actor_rollout_ref.ref.fsdp_config.param_offload=False \
     trainer.critic_warmup=0 \
     trainer.val_before_train=False \
-    trainer.n_gpus_per_node=2 \
+    trainer.n_gpus_per_node=4 \
     trainer.nnodes="${NNODES}" \
     trainer.logger=['console','wandb'] \
     trainer.project_name="${project_name}" \
